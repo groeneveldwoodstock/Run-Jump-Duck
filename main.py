752b8bfad19deab57ad5a19a647a68dd34933b63
Richard Groeneveld <42473836+groeneveldwoodstock@users.noreply.github.com>
2021-11-16 09:06:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,7 @@ class player(object):
     jump = [pygame.image.load(os.path.join('images', str(x) + '.png')) for x in range(1, 8)]
     slide = [pygame.image.load(os.path.join('images', 'S1.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')),pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S2.png')), pygame.image.load(os.path.join('images', 'S3.png')), pygame.image.load(os.path.join('images', 'S4.png')), pygame.image.load(os.path.join('images', 'S5.png'))]
     fall = pygame.image.load(os.path.join('images', '0.png'))
-    jumpList = [1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,-1,-1,-1,-1,-1,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-2,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-3,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4,-4]
-
+    jumpList = [3,3,3,3,3,3,3,3,3,3,2,3,2,3,2,3,2,3,2,3,2,2,2,2,2,2,2,2,2,2,1,2,1,2,1,2,1,2,1,2,1,1,1,1,1,1,1,1,1,1,1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,]
     def __init__(self, x, y, width, height):
         self.x = x
         self.y = y
@@ -37,16 +36,20 @@ class player(object):
 
     def draw(self, win):
         if self.falling:
-            win.blit(self.fall, (self.x, self.y + 30))
+            win.blit(self.fall, (self.x, self.y -50))
         elif self.jumping:
-            self.y -= self.jumpList[self.jumpCount] * 1.9
-            win.blit(self.jump[self.jumpCount//18], (self.x, self.y))
+            if self.jumpCount < 100:
+                self.y -= self.jumpList[self.jumpCount]
+            elif self.jumpCount >= 100:
+                self.y += self.jumpList[100 - self.jumpCount]
+            win.blit(self.jump[self.jumpCount//30], (self.x, self.y))
             self.jumpCount += 1
-            if self.jumpCount > 108:
+            if self.jumpCount >= 200:
                 self.jumpCount = 0
                 self.jumping = False
                 self.runCount = 0
-            self.hitbox = (self.x+ 4, self.y, self.width-24, self.height-10)
+                self.y = 315
+            self.hitbox = (self.x, self.y, self.width-24, self.height-10)
         elif self.sliding or self.slideUp:
             if self.slideCount < 20:
                 self.y += 1
